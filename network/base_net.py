f2ac5c0179a7d3dc40f3af6b047de64bad08bf4e
4ever-Rain <495644903@qq.com>
2022-02-28 13:56:47 +0000

    第一个版本的transformer

--- network/base_net.py
+++ network/base_net.py
@@ -42,23 +42,32 @@ class TRANSFORER(nn.Module):
     def __init__(self, input_shape, args):
         super(TRANSFORER, self).__init__()
         self.args = args
-
+        # self.seq_length = 10
+        # self.seq = []
+        # for _ in range(self.seq_length):
+        #     self.seq.append(torch.rand_like(input_shape))
         self.fc1 = nn.Linear(input_shape, args.rnn_hidden_dim)
-        if args.cuda:
-            self.pos_emb = torch.rand(self.args.rnn_hidden_dim).cuda()
-        else:
-            self.pos_emb = torch.rand(self.args.rnn_hidden_dim)
+
+        self.pos_emb = nn.Parameter(torch.rand(1, 1, self.args.rnn_hidden_dim))
+        # self.pos_emb = nn.Parameter(torch.rand(1, self.args.rnn_hidden_dim))
         self.encoder_layer = nn.TransformerEncoderLayer(args.rnn_hidden_dim, nhead=8)
         self.transformer_encoder = nn.TransformerEncoder(self.encoder_layer, num_layers=6)
         self.fc2 = nn.Linear(args.rnn_hidden_dim, args.n_actions)
 
     def forward(self, obs):
-        print("=======================")
-        print(obs.shape)
+        # if len(self.seq) <= self.seq_length:
+        #     self.seq.append(obs)
+        # 判断
+        # 组成队列
+        # （batch，seq，embin）
+        # torch.cat / stack
+        # transformer_encoder要求输入为(N, seq， embeding)
+        # 第一种解决方案，直接加一个维度加为seq(1)
+        # torch.autograd.set_detect_anomaly(True)
         x = f.relu(self.fc1(obs))
-        print("=======================")
-        print(x.shape)
-        x += self.pos_emb
+        x = x.unsqueeze(dim=1)
+        x = x + self.pos_emb
         h = self.transformer_encoder(x)
+        h = h.squeeze(dim=1)
         q = self.fc2(h)
         return q
